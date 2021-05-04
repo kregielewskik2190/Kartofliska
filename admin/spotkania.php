28f23a39045c5fc169e0eb3580f39ab04064d37e
kregielewskik2190 <74856684+kregielewskik2190@users.noreply.github.com>
2021-05-04 15:15:01 +0200

    KAR-51, KAR-54

--- admin/spotkania.php
+++ admin/spotkania.php
@@ -1,181 +1,143 @@
 <?php
 
+
 $visibility = 1;
 $minRole = 1;
-// session_start();
-
-$dir = __DIR__ . '/img/';
-
-
 require_once('../core/bootstrap.php');
 
-
 if (isset($_POST['action']) && $_POST['action'] === 'create') {
-		$Nazwa = $_POST['Nazwa'];
-		$Skrot =  $_POST['Skrot'];
-		$Liga =  (int) $_POST['Liga'];
-		$Miejscowosc = $_POST['Miejscowosc'];
 
-		$Herb = "";
+	$err = false;
 
-		if (!empty($_FILES['Herb']['name'])) {
-			$fName = $_FILES['Herb']['name'];
-			$salt = md5($Nazwa.$Skrot.$Miejscowosc);
-			$Herb = basename($salt."_".$fName);
-			$file = $dir . $Herb;
-			move_uploaded_file($_FILES['Herb']['tmp_name'], $file);
-		}
+	$liga = $_POST['liga'];
+	$druzyna_a = $_POST['druzyna_a'];
+	$druzyna_b = $_POST['druzyna_b'];
+	$data = $_POST['data'];
+	$kolejka = $_POST['kolejka'];
 
-		$err = false;
+	if (empty($liga)) {
+		$err = true;
+		echo "Pole liga nie może być puste!\r\n";
+	}
 
-		if (!isset($Nazwa) || empty($Nazwa)) {
-			$err = true;
-			echo "Podaj nazwę klubu ";
-			echo "<br>";
-		}
+	if (empty($druzyna_a) || empty($druzyna_b)) {
+		$err = true;
+		echo "Nazwy drużyn nie mogą być puste!\r\n";
+	}
 
-		if ((!isset($Skrot) || empty($Skrot))) {
-			$err = true;
-			echo "Podaj skrót nazwy klubu\r\n";
-			echo "<br>";
-		}
+	if (empty($data)) {
+		$err = true;
+		echo "Pole daty jest puste!\r\n";
+	}
 
-		if (isset($Skrot) && strlen($Skrot) > 3) {
-			$err = true;
-			echo "Skrót nazwy musi mieć max. 3 znak\r\n";
-			echo "<br>";
-		}
+	if ($druzyna_a == $druzyna_b) {
+		$err = true;
+		echo "Nazwy drużyn są identyczne\r\n";
+	}
 
-		if (!isset($Liga) || empty($Liga)) {
-			$err = true;
-			echo "Pole Liga jest puste \r\n ";
-			echo "<br>";
-		}
+	if (empty($kolejka)) {
+		$err = true;
+		echo "Brak kolejki\r\n";
+	}
 
-		if (isset($Liga) && !in_array($Liga, range(1,5)) ) {
-			$err = true;
-			echo "Wartość pola Liga jest podana nieprawidłowo\r\n";
-			echo "<br>";
-		}
+	if (!$err)
+		$app->createSpotkania(
+			$liga,
+			$druzyna_a,
+			$druzyna_b,
+			$data,
+			$kolejka
+		);
 
-				if (!isset($Miejscowosc) || empty($Miejscowosc)) {
-			$err = true;
-				echo "Podaj nazwę miejscowości \r\n ";
-			echo "<br>";
-		}
-		if (!$err)
-			$app->createDruzyna(
-				$Nazwa,
-				$Skrot,
-				$Herb,
-				$Liga,
-				$Miejscowosc
-			);
-}
-if (isset($_POST['action']) && $_POST['action'] === 'edit') {
-	$editDruzyna = $app->getDruzyna($_POST['ID']);
-	if (!$editDruzyna) return;
-		$ID = $_POST['ID'];
-		$Nazwa = $_POST['Nazwa'];
-		$Skrot =  $_POST['Skrot'];
-		$Liga =  (int) $_POST['Liga'];
-		$Miejscowosc = $_POST['Miejscowosc'];
+	} else if (isset($_POST['action']) && $_POST['action'] === 'edit') {
+	$editSpotkania = $app->getSpotkania($_POST['ID']);
+	if ($editSpotkania) {
+		$app->updateSpotkanie(
+			$_POST['ID'],
+			$_POST['druzyna_a'],
+			$_POST['druzyna_b'],
+			$_POST['data'],
+			$_POST['bramkiA'],
+			$_POST['bramkiB'],
+			$_POST['liga'],
+			$_POST['kolejka']
+		);
+		header('Location: spotkania.php');
 
-		$Herb = $_FILES["Herb"]["name"];
-
-		if (!empty($Herb)) {
-			$file = $dir . basename($_FILES['Herb']['name']);
-			move_uploaded_file($_FILES['Herb']['tmp_name'], $file);
-		}
-
-		$err = false;
-
-		if (!isset($Nazwa) || empty($Nazwa)) {
-			$err = true;
-			echo "Podaj nazwę klubu ";
-			echo "<br>";
-		}
-
-		if ((!isset($Skrot) || empty($Skrot))) {
-			$err = true;
-			echo "Podaj skrót nazwy klubu\r\n";
-			echo "<br>";
-		}
-
-		if (isset($Skrot) && strlen($Skrot) > 3) {
-			$err = true;
-			echo "Skrót nazwy musi mieć max. 3 znak\r\n";
-			echo "<br>";
-		}
-
-		if (!isset($Liga) || empty($Liga)) {
-			$err = true;
-			echo "Pole Liga jest puste \r\n ";
-			echo "<br>";
-		}
-
-		if (isset($Liga) && !in_array($Liga, range(1,5)) ) {
-			$err = true;
-			echo "Wartość pola Liga jest podana nieprawidłowo\r\n";
-			echo "<br>";
-		}
-
-		if (!isset($Miejscowosc) || empty($Miejscowosc)) {
-			$err = true;
-				echo "Podaj nazwę miejscowości \r\n ";
-			echo "<br>";
-		}
-
-		if (!$err)
-			$app->updateDruzyna($ID, $Nazwa, $Skrot, $Herb, $Liga, $Miejscowosc);
+	}
 }
 
-if (isset($_GET['remove']) && $_GET['remove'] > 0 && $_GET['remove'] !== $druzyna['ID']) {
-	$app->removeDruzyna($_GET['remove']);
-	header('Location: druzyna.php');
+
+if (isset($_GET['remove']) && $_GET['remove'] > 0 && $_GET['remove'] !== $spotkania['id']) {
+	$app->removeSpotkania($_GET['remove']);
+	header('Location: spotkania.php');
 }
 if (isset($_GET['edit'])) {
-	$editDruzyna = $app->getDruzyna($_GET['edit']);
-	if (!$editDruzyna) {
-		header('Location: druzyna.php');
+	$editSpotkania = $app->getSpotkania($_GET['edit']);
+	if (!$editSpotkania) {
+		header('Location: spotkania.php');
 		exit();
 	}
 }
-$druzyna = $app->getAllDruzyna();
+$spotkania = $app->getAllSpotkania();
 ?>
-<form action="druzyna.php" method="POST" enctype="multipart/form-data">
-	<input type="hidden" name="ID" value="<?= isset($editDruzyna) ? $editDruzyna['ID'] : (isset($err) ?  $_POST['ID'] :  '') ?>" />
-	<input type="text" name="Nazwa" value="<?= isset($editDruzyna) ?  $editDruzyna['Nazwa'] : (isset($err) ?  $_POST['Nazwa'] : '') ?>" placeholder="Nazwa" />
-	<input type="text" name="Skrot" value="<?= isset($editDruzyna) ?  $editDruzyna['Skrot'] : (isset($err) ?  $_POST['Skrot'] : '')  ?>" placeholder="Skrot" />
-	<input type="file" name="Herb" accept=".png, .jpeg, .jpg"/>
-	<input type="text" name="Liga" value="<?= isset($editDruzyna) ? $editDruzyna['Liga'] : (isset($err) ?  $_POST['Liga'] : '')  ?>" placeholder="Liga" />
-	<input type="text" name="Miejscowosc" value="<?= isset($editDruzyna) ? $editDruzyna['Miejscowosc'] : (isset($err) ?  $_POST['Miejscowosc'] : '')?>" placeholder="Miejscowosc" />
-	<input type="hidden" name="action" value="<?= isset($editDruzyna) ? "edit" : 'create' ?>">
-	<input type="submit" value="<?= isset($editDruzyna) ? 'Edytuj' : 'Utwórz' ?> drużynę" />
+<form action="spotkania.php" method="POST" enctype="multipart/form-data">
+	<input type="hidden" name="ID" value="<?= isset($editSpotkania) ? $editSpotkania['id'] : '' ?>" />
+	<input type="number" name="liga" value="<?= isset($editSpotkania) ? $editSpotkania['liga'] : '' ?>" placeholder="liga" min="1" max="5" required/>
+	<label for="druzyna_a">drużyna a:</label>
+	<select id="druzyna_a" name="druzyna_a" required>
+		<?php $teams = $app->getAllNamesDruzyna();
+		foreach ($teams as $key => $team) {
+			$nazwa = $team['Nazwa'];
+
+			$option = '<option value="'.$nazwa.'"'.(isset($editSpotkania) && $editSpotkania["druzyna_a"] == $nazwa ?'selected' : '').'>'.$nazwa.'</option>';
+			echo $option;
+		} ?>
+	</select>
+	<label for="druzyna_b">drużyna b:</label>
+	<select id="druzyna_b" name="druzyna_b" required>
+		<?php $teams = $app->getAllNamesDruzyna();
+		foreach ($teams as $key => $team) {
+			$nazwa = $team['Nazwa'];
+			$option = '<option value="'.$nazwa.'"'.(isset($editSpotkania) && $editSpotkania["druzyna_b"] == $nazwa ?'selected' : '').'>'.$nazwa.'</option>';
+			echo $option;
+		} ?>
+	</select>
+	<input type="date" name="data" value="<?= isset($editSpotkania) ? $editSpotkania['data'] : '' ?>" placeholder="data" required/>
+	<input type="number" name="bramkiA" value="<?= isset($editSpotkania) ? $editSpotkania['bramkiA'] : '' ?>" placeholder="Liczba bramek drużyny A" <?= isset($editSpotkania) ? '' : 'disabled="disabled"' ?> />
+	<input type="number" name="bramkiB" value="<?= isset($editSpotkania) ? $editSpotkania['bramkiB'] : '' ?>" placeholder="Liczba bramek drużyny A" <?= isset($editSpotkania) ? '' : 'disabled="disabled"' ?> />
+	<input type="number" name="kolejka" value="<?= isset($editSpotkania) ? $editSpotkania['kolejka'] : '' ?>" placeholder="Kolejka" min="1" max="30"/>
+	<input type="hidden" name="action" value="<?= isset($editSpotkania) ? "edit" : 'create' ?>">
+
+	<input type="submit" value="<?= isset($editSpotkania) ? 'Edytuj' : 'Utwórz' ?> spotkanie" />
 
 </form>
 <br /><a href="menu.php">Powrót</a>
 <hr>
 <table>
 	<tr>
-		<td>ID</td>
-		<td>Nazwa</td>
-		<td>Skrot</td>
-		<td>Herb</td>
+		<td>id</td>
 		<td>Liga</td>
-		<td>Miejscowosc</td>
+		<td>druzyna_a</td>
+		<td>druzyna_b</td>
+		<td>data</td>
+		<td>bramki A</td>
+		<td>bramki B</td>
+		<td>Kolejka</td>
 	</tr>
 	<?php
-	foreach ($druzyna as $druzyna_) {
+	foreach ($spotkania as $spotkania_) {
 		echo '<tr>
-<td>' . $druzyna_['ID'] . '</td>
-<td>' . $druzyna_['Nazwa'] . '</td>
-<td>' . $druzyna_['Skrot'] . '</td>
-<td>' . ' <img width=64 src="/admin/img/'. $druzyna_['Herb'].'" '. ' alt=" "></td>
-<td>' . $druzyna_['Liga'] . '</td>
-<td>' . $druzyna_['Miejscowosc'] . '</td>
+<td>' . $spotkania_['id'] . '</td>
+<td>' . $spotkania_['liga'] . '</td>
+<td>' . $spotkania_['druzyna_a'] . '</td>
+<td>' . $spotkania_['druzyna_b'] . '</td>
+<td>' . $spotkania_['data'] . '</td>
+<td>' . $spotkania_['bramkiA'] . '</td>
+<td>' . $spotkania_['bramkiB'] . '</td>
+<td>' . $spotkania_['kolejka'] . '</td>
 
-<td><a href="?remove=' . $druzyna_['ID'] . '">Usuń</a> <a href="?edit=' . $druzyna_['ID'] . '">Edytuj</a></td>
+<td><a href="?remove=' . $spotkania_['id'] . '">Usuń</a> <a href="?edit=' . $spotkania_['id'] . '">Edytuj</a></td>
 </tr>';
 	}
 	?>
